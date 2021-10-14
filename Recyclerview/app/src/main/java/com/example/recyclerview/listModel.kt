583609d856faa9fca7b2f5ba2dd2c07e5d9b1c4c
ToTheN <tushar.saini@tothenew.com>
2021-10-14 18:33:16 +0530

    recyclerview comitted

---
+++ Recyclerview/app/src/main/java/com/example/recyclerview/listModel.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerview
+
+import android.widget.ImageView
+import android.widget.RatingBar
+
+data class listModel(val descrip:String,val dateTime:String,val image:Int,val Amount:String,val rate:Int) {
+
+}
